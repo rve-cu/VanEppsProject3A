b3400ac75efd8d20e505719b781ea33b3d70d530
Rebekah Van Epps <rebekah.vanepps@ucdenver.edu>
2018-10-22 18:36:34 -0600

    initial commit - working repo

---
+++ VanEppsProject3A/frmFixedMortgageCalculator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VanEppsProject3A
+{
+    public partial class frmFixedMortgageCalculator : Form
+    {
+        public frmFixedMortgageCalculator()
+        {
+            InitializeComponent();
+        }
+
+        frmRateCalculator formRateCalculator;
+
+        private void btnCalculateMortgage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Convert input
+                double principalAmount = Convert.ToDouble(txtPrincipalAmount.Text);
+                int termLength = Convert.ToInt32(txtTermLength.Text);
+                double annualRate = Convert.ToDouble(txtAnnualRate.Text);
+
+                // Only allow principals of $1,000,000 or less
+                if (principalAmount >= 1000000)
+                {
+                    throw new Exception("Please enter a value less than 1,000,000 for Principal.");
+                }
+
+                // Only allow terms lengths between 5 and 30 years
+                if (termLength < 5 || termLength > 30)
+                {
+                    throw new Exception("Invalid term length. Please enter a value of at least 5 years and no more than 30 years.");
+                }
+
+                // Only allow positive values for interest rate
+                if (annualRate <= 0)
+                {
+                    throw new ArithmeticException("Interest rate cannot be zero or negative. Please enter a positive value or use the Interest Rate Calculator form.");
+                }
+
+                // Only allow positive values for principal
+                if (principalAmount <= 0)
+                {
+                    throw new ArithmeticException("Principal cannot be zero or negative. Please enter a positive value.");
+                }
+
+                // Convert interest rate and term length to monthly amounts
+                double monthlyRate = annualRate / 12 / 100; // Divide annualRate by 12 (months in year) then divide by 100 to get decimal rate value
+                int termLengthMonths = termLength * 12;
+
+                // Calculate monthly payment
+                double monthlyPayment = (monthlyRate * principalAmount) / (1 - Math.Pow(1 + monthlyRate, -termLengthMonths));
+                // Calculate total amount owed
+                double totalOwed = monthlyPayment * termLengthMonths;
+                // Calculate interest paid
+                double totalInterest = totalOwed - principalAmount;
+
+                // Format and display calculated values
+                txtMonthlyPayment.Text = monthlyPayment.ToString("c");
+                txtTotalOwed.Text = totalOwed.ToString("c");
+                txtTotalInterest.Text = totalInterest.ToString("c");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(
+                    "Invalid input format. Please enter only numeric values and don't leave any fields blank.",
+                    "Format Error");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show(
+                    "Please enter a smaller numeric value.",
+                    "Overflow Error");
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show(
+                    "Cannot divide by zero. Please enter a value greater than zero.",
+                    "Division By Zero Error");
+            }
+            catch (ArithmeticException ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    "Arithmetic Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    ex.GetType().ToString()
+                    );
+            }
+        }
+
+        private void btnExitMortgage_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRate_Click(object sender, EventArgs e)
+        {
+            // Display Interest Rate Calculator form
+            formRateCalculator = new frmRateCalculator(this);
+            formRateCalculator.Show();
+        }
+    }
+}
---
+++ VanEppsProject3A/frmRateCalculator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VanEppsProject3A
+{
+    public partial class frmRateCalculator : Form
+    {
+        Form formFixedMortgageCalculator;
+
+        public frmRateCalculator(frmFixedMortgageCalculator _form)
+        {
+            InitializeComponent();
+            this.formFixedMortgageCalculator = _form; // pull in current instance of fixed-rate mortgage calculator form
+        }
+
+        private void btnCalculateRate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Convert input
+                int creditScore = Convert.ToInt32(txtCreditScore.Text);
+
+                if (creditScore == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+
+                // Only allow credit scores between 300 and 850
+                if (creditScore < 300 || creditScore > 850)
+                {
+                    throw new Exception("Invalid credit score. Please enter a value between 300 and 850.");
+                }
+
+                // Determine interest rate based on credit score
+                double interestRate = 0; // Initialize interest rate var
+                // Arbitrarily determine interest rate by dividing 3000 by credit score
+                interestRate = 3000.0 / creditScore;
+
+                // Format and display calculated value
+                txtInterestRate.Text = interestRate.ToString("f3");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(
+                    "Invalid input format. Please enter only numeric values and don't leave any fields blank.",
+                    "Format Error");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show(
+                    "Please enter a smaller numeric value.",
+                    "Overflow Error");
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show(
+                    "Cannot divide by zero. Please enter a value greater than zero.",
+                    "Division By Zero Error");
+            }
+            catch (ArithmeticException)
+            {
+                MessageBox.Show(
+                    "Please enter a smaller, positive numeric value.",
+                    "Arithmetic Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    ex.GetType().ToString()
+                    );
+            }
+        }
+
+        private void btnExitRate_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCopyRate_Click(object sender, EventArgs e)
+        {
+            // Copy current value for interest rate to corresponding input on fixed-rate mortgage calculator form
+            formFixedMortgageCalculator.Controls.Find("txtAnnualRate", true).First().Text = txtInterestRate.Text;
+        }
+    }
+}
